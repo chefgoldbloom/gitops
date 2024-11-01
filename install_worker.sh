b98c237621e8ff0472f86a8131dc22661c49e24a
Alex Sylvis <alexsylvis@hey.com>
2024-11-01 11:22:34 -0700

    Create install_worker.sh
    add install_worker.sh

---
+++ install_worker.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Set up System
+sudo hostnamectl set-hostname control-plane
+sudo swapoff -a
+sudo ufw disable
+sudo apt-get update
+
+
+# Install Containerd & Docker
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enable IPv4 Packet Forwarding
+echo 'Enabling IPv4 packet forwarding...'
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+
+sudo sysctl --system | grep net.ipv4.ip_forward
+
+# Overwrite /etc/containerd/config.toml with default config
+echo 'Overwriting /etc/containerd/config.toml...'
+sudo containerd config default > /etc/containerd/config.toml
+
+# Configure systemd cgroup driver
+echo 'Configure systemd cgroup driver...'
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+## Install Kubeadm, Kubelet, Kubectl
+echo 'Installing Kubeadm, Kubelet, and Kubectl'
+sudo apt-get -y install gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
